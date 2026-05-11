1a942f6b18a4b1e91f961780f414e0eac79c1e2c
Alejovar <alejovar.0208@gmail.com>
2026-05-11 17:09:00 -0600

    help

--- docker/migration_users_autoincrement.sql
+++ docker/migration_users_autoincrement.sql
@@ -5,5 +5,16 @@
 
 USE KitchenLink;
 
+SET FOREIGN_KEY_CHECKS = 0;
+
+ALTER TABLE `attendance_records`
+  DROP FOREIGN KEY `fk_attendance_user`;
+
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `attendance_records`
+  ADD CONSTRAINT `fk_attendance_user`
+    FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+SET FOREIGN_KEY_CHECKS = 1;
